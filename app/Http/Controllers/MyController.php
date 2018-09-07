a9f97a8f2ce081a00d1d11eaee9d36838fc3cb55
curioushoainam <hoainamtruongute@gmail.com>
2018-09-07 16:25:35 +0700

    first commit

---
+++ app/Http/Controllers/MyController.php
@@ -0,0 +1,18 @@
+<?php
+// lệnh tạo controller từ cmd folder chủ 
+// php artisan make:controller MyController
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyController extends Controller
+{
+    public function Hello($name){
+    	echo "Hallo ".$name;
+    }
+
+    public function Farewell(){
+    	echo "Auf Wiedersehen VHD";
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// hnhd code
+Route::get('HoangDiem', function () {
+    return 'Miss you so much !';
+});
+
+Route::get('HoangDiem/love', function () {
+    return 'Do you love me ?';
+});
+
+// truyen tham so
+Route::get('HoTen/{ten}', function($ten){
+	return 'Her name is ' . $ten;
+});
+
+Route::get('today/{ten}', function($ten){
+	echo 'Her name is ' . $ten;
+})->where(['ten'=>'[a-zA-Z ]+']);		
+// ràng buộc kiểu dữ liệu của tham số truyền
+// trong trường hợp này ngay phải là ký tự a-z hoặc A-Z
+
+// ================================
+// Đinh danh Route
+Route::get('Route1', function(){
+	return 'Hallo leuter ';
+})->name('hallo1');
+
+Route::get('Route2', ['as'=>'hallo', function(){
+	return "Guten Tag. Wie geht's ? Mir geht gut.";
+}]);
+
+
+Route::get('Call', function(){
+	return redirect()->route('hallo');		// Gọi route có tên là hallo // giống hàm chuyển trang header
+});
+
+// ================================
+// Route Group
+Route::group(['prefix'=>'MyGroup'], function(){
+	// Call User1 => domain/MyGroup/User1
+	Route::get('User1', function(){
+		return 'Ich bin User1. Und du?';
+	});
+	// Call User2 => domain/MyGroup/User2
+	Route::get('User2', function(){
+		return 'Ich bin User2. Und du?';
+	});
+	// Call User3 => domain/MyGroup/User3
+	Route::get('User3', function(){
+		return 'Ich bin User3. Und du?';
+	});
+});
+
+// ================================
+// Gọi controller MyController
+Route::get('CallController/{name}','MyController@Hello');
+
+Route::get('CallController2','MyController@Farewell');
+
