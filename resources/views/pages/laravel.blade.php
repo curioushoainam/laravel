1e21f752da115f5a853fb3ff79949c755a116062
curioushoainam <hoainamtruongute@gmail.com>
2018-09-11 11:00:20 +0700

    demo foreach forelse continue break

--- resources/views/pages/laravel.blade.php
+++ resources/views/pages/laravel.blade.php
@@ -8,7 +8,7 @@
 <br>
 {{-- {!!$course!!} --}}
 
-@if(@isset($course))
+{{-- @if(@isset($course))
 	{!!$course!!}
 
 @else
@@ -25,7 +25,44 @@
 	<li>{{$i2}}</li>
 	
 	@php($i3 = $i*3)
-	<li>{{$i3}}</li>
-	
-@endfor
+	<li>{{$i3}}</li>	
+@endfor --}}
+
+{{-- @php() = @php @endphp // --}}
+@php($arr = array("HTML","CSS","JS","PHP","Laravel"))
+
+{{-- @if(!empty($arr))
+	@foreach ($arr as $val)
+		{{$val}}
+	@endforeach	
+@else
+	{{'Array is empty !'}}
+
+@endif --}}
+
+@forelse($arr as $val)
+{{-- forelse = if(!empty()) foreach($arr as $item) --}}	
+	{{$val}}
+@empty
+	{{'Array is empty !'}}
+@endforelse
+
+<br>
+@forelse($arr as $val)
+{{-- ignore JS --}}
+	@continue($val == 'JS')
+	{{$val}}
+@empty
+	{{'Array is empty !'}}
+@endforelse
+
+<br>
+@forelse($arr as $val)
+{{-- stop once == JS --}}
+	@break($val == 'JS')
+	{{$val}}
+@empty
+	{{'Array is empty !'}}
+@endforelse
+
 @endsection
