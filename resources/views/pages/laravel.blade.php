1348ab329aeb83850ed821dd6c8c01ecd14ff8c8
curioushoainam <hoainamtruongute@gmail.com>
2018-09-11 10:27:12 +0700

    demo if-else and for loop

--- resources/views/pages/laravel.blade.php
+++ resources/views/pages/laravel.blade.php
@@ -3,12 +3,29 @@
 @section('contents')
 <h5>laravel</h5>
 
-<?= $course ?>
+<br>
+{{-- {{$course}}  DO NOT compile html code --}}
+<br>
+{{-- {!!$course!!} --}}
+
+@if(@isset($course))
+	{!!$course!!}
+
+@else
+	{{'Không tồn tại $course!'}}
+
+@endif
 
 <br>
-{{$course}}  {{-- DO NOT compile html code --}}
+{!!'3 persons => '!!}{!!$course or 'Không tồn tại $course!'!!}
 
 <br>
-{!!$course!!}
-
+@for($i=0; $i<5; $i++)
+	@php($i2 = $i*2)	
+	<li>{{$i2}}</li>
+	
+	@php($i3 = $i*3)
+	<li>{{$i3}}</li>
+	
+@endfor
 @endsection
